7d5f3f4d548fa1f1dacc3a2e4cd471e313a6a541
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-31 23:21:39 +0530

    [Mukhesh].Add Method to calculate fare

---
+++ CabInvoiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoice
+{
+    /// <summary>
+    /// This class contain various Custom exceptions
+    /// </summary>
+        [Serializable]
+        public class CabInvoiceException : Exception
+        {
+            public ExceptionType exceptionType;
+            public enum ExceptionType
+            {
+                INVALID_RIDE_TYPE,
+                INVALID_DISTANCE,
+                INVALID_TIME,
+            }
+            public CabInvoiceException(ExceptionType exception, string message) : base(message)
+            {
+                this.exceptionType = exception;
+            }
+        }
+}
